bf53a978a9f565f710077cdcb316fef7a2c11242
Alexander Veicht <veichta@ethz.ch>
2024-01-17 16:00:41 +0100

    Add option to filter by group-id.

--- flickr_downloader.py
+++ flickr_downloader.py
@@ -3,7 +3,9 @@ import concurrent.futures
 import json
 import os
 from io import BytesIO
+from pathlib import Path
 
+import pandas as pd
 import requests
 from flickrapi import FlickrAPI
 from PIL import Image
@@ -12,9 +14,11 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_dir", type=str, default="data", help="data directory")
 parser.add_argument("--secrets", type=str, default="secrets.json", help="secrets file")
-parser.add_argument("--num_images", "-n", type=int, default=10, help="number of images")
+parser.add_argument("--num_pages", "-n", type=int, help="number of images")
 parser.add_argument("--num_workers", type=int, default=4, help="number of workers")
-parser.add_argument("--keywords", "-k", nargs="+", type=str, required=True, help="keywords")
+parser.add_argument("--keywords", "-k", nargs="+", default="", type=str, help="keywords")
+parser.add_argument("--group_id", "-gid", type=str, default="", help="Group ID")
+parser.add_argument("--download_only", action="store_true", help="download only")
 args = parser.parse_args()
 
 
@@ -25,91 +29,176 @@ SECRET = secrets["secret"]
 
 SIZES = ["url_o", "url_k", "url_h", "url_l", "url_c"]  # in order of preference
 
+Image.MAX_IMAGE_PIXELS = 200000000
 
-def get_photos(image_tag, page=1, per_page=100):
+
+def get_licenses():
     flickr = FlickrAPI(KEY, SECRET, format="parsed-json")
+    license_infos = flickr.photos.licenses.getinfo()
+    licenses = license_infos["licenses"]["license"]
 
+    return {str(license["id"]): license["name"] for license in licenses}
+
+
+def get_photos(image_tag, group_id, flickr, page=1):
     return flickr.photos.search(
         text=image_tag,
         tag_mode="all",
         # tags=image_tag,
         extras=",".join(SIZES),
         page=page,
-        per_page=per_page,
+        group_id=group_id,
+        per_page=100,
         sort="relevance",
     )
 
 
-def get_url(photo):
-    for i in range(len(SIZES)):  # makes sure the loop is done in the order we want
-        url = photo.get(SIZES[i])
-        if url:  # if url is None try with the next size
-            return url
+def get_image_infos(photo, licenses, flickr):
+    owner_id = photo["owner"]
+
+    owner_info = flickr.people.getInfo(user_id=owner_id)
+
+    owner_url = owner_info["person"]["profileurl"]["_content"]
+    owner_name = owner_info["person"]["username"]["_content"]
+
+    photo_id = photo["id"]
+
+    title = photo["title"]
+
+    photo_info = flickr.photos.getInfo(photo_id=photo_id, secret=photo["secret"])
+
+    license_id = photo_info["photo"]["license"]
+    license = licenses[license_id]
+
+    size = next(
+        (size for size in SIZES if size in photo.keys()),
+        None,
+    )
+
+    url = photo[size]
+
+    size = size.replace("url_", "")
+
+    h, w = photo[f"height_{size}"], photo[f"width_{size}"]
+
+    return {
+        "owner_id": owner_id,
+        "owner_url": owner_url,
+        "owner_name": owner_name,
+        "photo_id": photo_id,
+        "title": title,
+        "height": h,
+        "width": w,
+        "license_id": license_id,
+        "license": license,
+        "url": url,
+    }
 
 
-def get_urls(image_tag, n_images):
-    n_pages = get_photos(image_tag)["photos"]["pages"]
+def process_page(image_tag, group_id, page, licenses, flickr, out_dir):
+    photos = get_photos(image_tag, group_id, flickr, page=page)["photos"]["photo"]
 
-    counter = 0
-    urls = []
+    infos = []
+    pbar = tqdm(total=len(photos), desc=f"Page {page:04d}")
+    for photo in photos:
+        try:
+            row = get_image_infos(photo, licenses, flickr)
+        except Exception as e:
+            print(f"Faild to get info for {photo['id']}")
+            continue
 
-    pbar = tqdm(total=n_images)
-    for i in range(n_pages):
-        photos = get_photos(image_tag, page=i + 1)["photos"]["photo"]
-        for photo in photos:
-            if counter < n_images:
-                url = get_url(photo)  # get preffered size url
-                if url:
-                    urls.append(url)
-                    counter += 1
-                    pbar.update(1)
-                # if no url for the desired sizes then try with the next photo
-            else:
-                break
-
-        if counter >= n_images:
-            break
+        infos.append(row)
+        pbar.update(1)
 
     pbar.close()
 
-    return urls
+    df = pd.DataFrame(infos)
+
+    df.to_csv(os.path.join(out_dir, f"page-{page:04d}.csv"), index=False)
 
 
 def dowload(url, out, pbar=None):
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
-    # convert to RGB
-    img = img.convert("RGB")
-    img.save(out)
+    if out.exists():
+        if pbar:
+            pbar.update(1)
+        return
 
-    if pbar:
-        pbar.update(1)
+    try:
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        # convert to RGB
+        img = img.convert("RGB")
+        img.save(out)
 
+        if pbar:
+            pbar.update(1)
+
+    except Exception as e:
+        print(f"Failed to download {url}")
+
+
+# SETUP
+flickr = FlickrAPI(KEY, SECRET, format="parsed-json")
 
 keyword = " ".join(args.keywords)
 
-print("Getting urls...")
-urls = get_urls(keyword, args.num_images)
-print("Done!")
 
-print("Downloading images...")
+n_pages = get_photos(keyword, args.group_id, flickr)["photos"]["pages"]
+
+if args.num_pages:
+    n_pages = min(n_pages, args.num_pages)
+
+print(f"Using {n_pages} pages.")
 
 
-# set up output directory
-out_keyword = keyword.replace(" ", "_")
-out_dir = os.path.join(args.data_dir, out_keyword)
+out_dir = Path(args.data_dir)
+if not keyword:
+    dir_name = input("Enter directory name: ")
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+    out_dir = out_dir / dir_name
+else:
+    out_dir = out_dir / keyword.replace(" ", "_")
 
-# download images update progress bar
-pbar = tqdm(total=len(urls))
-with concurrent.futures.ThreadPoolExecutor(args.num_workers) as executor:
-    futures = [
-        executor.submit(dowload, url, os.path.join(out_dir, f"{i}.jpg"), pbar)
-        for i, url in enumerate(urls)
-    ]
+if not out_dir.exists():
+    out_dir.mkdir()
 
-pbar.close()
+info_dir = out_dir / "info"
+if not info_dir.exists():
+    info_dir.mkdir()
 
-print("Done!")
+image_dir = out_dir / "images"
+if not image_dir.exists():
+    image_dir.mkdir()
+
+licenses = get_licenses()
+
+if not args.download_only:
+    with concurrent.futures.ThreadPoolExecutor(args.num_workers) as executor:
+        futures = [
+            executor.submit(process_page, keyword, args.group_id, pid, licenses, flickr, info_dir)
+            for pid in range(1, n_pages + 1)
+        ]
+
+    res = [future.result() for future in futures]
+
+
+for page in range(1, n_pages + 1):
+    page_dir = image_dir / f"page-{page:04d}"
+    if not page_dir.exists():
+        page_dir.mkdir()
+
+    df = pd.read_csv(info_dir / f"page-{page:04d}.csv")
+
+    urls = df["url"].values
+    img_ids = df["photo_id"].values
+
+    pbar = tqdm(total=len(urls), desc=f"Download images of page {page:04d}")
+    with concurrent.futures.ThreadPoolExecutor(args.num_workers) as executor:
+        futures = [
+            executor.submit(dowload, url, page_dir / f"{img_id}.jpg", pbar)
+            for url, img_id in zip(urls, img_ids)
+        ]
+
+    res = [future.result() for future in futures]
+
+    pbar.close()
